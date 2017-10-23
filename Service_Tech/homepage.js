a6972204a839801ab160b7d32a411ced396f0ab9
Stanley <snguyen414@gmail.com>
2017-10-23 18:16:26 -0500

    Add files via upload

---
+++ Service_Tech/homepage.js
@@ -0,0 +1,9 @@
+var $root = $('html, body');
+
+$('.navbar div > a').click(function () {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top - 139
+    }, 900);
+
+    return false;
+});
